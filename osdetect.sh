fd5c703bca29f784d79862d85bead07acaa9e2a7
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-11 20:58:36 +0700

    Update osdetect.sh

--- osdetect.sh
+++ osdetect.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
 set -e
 
-YUM_PACKAGE_NAME="update php"
-DEB_PACKAGE_NAME="update upgrade php"
+YUM_PACKAGE_NAME="php nginx net-tools"
+DEB_PACKAGE_NAME="php nginx net-tools" 
 
  if cat /etc/*release | grep ^NAME | grep CentOS; then
     echo "==============================================="
     echo "Installing packages $YUM_PACKAGE_NAME on CentOS"
     echo "==============================================="
-    yum install -y $YUM_PACKAGE_NAME
+    sudo yum update -y
+    sudo yum install -y $YUM_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Red; then
     echo "==============================================="
     echo "Installing packages $YUM_PACKAGE_NAME on RedHat"
     echo "==============================================="
-    yum install -y $YUM_PACKAGE_NAME
+    sudo yum update -y
+    sudo yum install -y $YUM_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Fedora; then
     echo "================================================"
     echo "Installing packages $YUM_PACKAGE_NAME on Fedorea"
     echo "================================================"
-    yum install -y $YUM_PACKAGE_NAME
+    sudo yum update -y
+    sudo yum install -y $YUM_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
     echo "==============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
     echo "==============================================="
-    apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    sudo apt-get update && sudo apt-get -y upgrade
+    sudo apt-get install -y $DEB_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Debian ; then
     echo "==============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Debian"
     echo "==============================================="
-    apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    sudo apt-get update && sudo apt-get -y upgrade
+    sudo apt-get install -y $DEB_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Mint ; then
     echo "============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Mint"
     echo "============================================="
-    apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    sudo apt-get update && sudo apt-get -y upgrade
+    sudo apt-get install -y $DEB_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Knoppix ; then
     echo "================================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Kanoppix"
     echo "================================================="
-    apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    sudo apt-get update && sudo apt-get -y upgrade
+    sudo apt-get install -y $DEB_PACKAGE_NAME
  else
     echo "OS NOT DETECTED, couldn't install package $PACKAGE"
     exit 1;
