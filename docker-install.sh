22fb3d980c8056662824d9dd8b04e641a51c09db
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-14 15:42:00 +0700

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,13 @@
+sudo apt update -y && apt upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo install -m 0755 -d /etc/apt/keyrings
+url='https://download.docker.com/linux/ubuntu/gpg'
+curl -fsSL $url | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker username
