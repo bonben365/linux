a4375180db5a142e6a4690c8b2e6003ac92d3a33
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-15 12:14:50 +0700

    Update kms.sh

--- kms.sh
+++ kms.sh
@@ -4,30 +4,29 @@ version=$(cat /etc/os-release | grep "PRETTY_NAME" | sed 's/PRETTY_NAME=//g' | s
 
 if [ $version == "Ubuntu" ]
 then
-    sudo apt-get install ca-certificates curl gnupg
-    sudo mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-    sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-    "deb [arch="$(dpkg --print-architecture)" \
-    signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y &&
-    sudo systemctl start docker
-    sudo systemctl status docker
+    sudo apt install git -y
+    cd /home
+    sudo git clone https://github.com/kebe7jun/linux-kms-server
+    cd /linux-kms-server/vlmcsd
+    cp vlmcsd /home && cd /home && mv vlmcsd kms
+    sudo ./kms -R170d -L 0.0.0.0:1688 -l /home/kms.log
+    firewall-cmd --zone=public --add-port=1688/tcp --permanent
+    echo "@reboot cd /home && sudo ./kms -R170d -L 0.0.0.0:1688 -l /home/kms.log" >> /etc/crontab
+    timedatectl set-timezone Asia/Ho_Chi_Minh
 fi
 
 
 if [ $version == "CentOS" ]
 then
-    sudo yum install -y yum-utils
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&
-    sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y &&
-    sudo systemctl start docker
-    sudo systemctl status docker
+    sudo yum install git -y
+    cd /home
+    sudo git clone https://github.com/kebe7jun/linux-kms-server
+    cd /linux-kms-server/vlmcsd
+    cp vlmcsd /home && cd /home && mv vlmcsd kms
+    sudo ./kms -R170d -L 0.0.0.0:1688 -l /home/kms.log
+    firewall-cmd --zone=public --add-port=1688/tcp --permanent
+    echo "@reboot cd /home && sudo ./kms -R170d -L 0.0.0.0:1688 -l /home/kms.log" >> /etc/crontab
+    timedatectl set-timezone Asia/Ho_Chi_Minh
 fi
 
 if [ $version == "Debian" ]
