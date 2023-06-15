a653023ad9673e9a5ba747796bab32b5c3e5e12c
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-15 18:21:33 +0700

    Update imapsync.sh

--- imapsync.sh
+++ imapsync.sh
@@ -8,7 +8,7 @@ then
 
   sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
   sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
-  sud dnf update -y
+  sudo dnf update -y
   sudo dnf install epel-release -y
 
   sudo yum groupinstall "Development Tools" -y
