38997185c42daf052494798a2588a9cb9e548c2f
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-15 21:44:56 +0700

    Update gui.sh

--- gui.sh
+++ gui.sh
@@ -9,8 +9,7 @@ then
   sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
   sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
   sudo dnf update -y
-  sudo dnf --enablerepo=epel,PowerTools group -y install "KDE Plasma Workspaces" "base-x" -y
-
+  sudo dnf groupinstall "Server with GUI" -y
   sudo systemctl set-default graphical
 fi
 
