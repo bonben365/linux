f3980c06188a35c6e014a28d0a1714f289a4c262
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-16 14:15:03 +0700

    Update imapsync-refresh.py

--- imapsync-refresh.py
+++ imapsync-refresh.py
@@ -9,7 +9,7 @@ url = "https://login.microsoftonline.com/common/oauth2/v2.0/token"
 params = {
   "grant_type":"refresh_token",
   "scope":"https://outlook.office365.com/.default",
-  "refresh_token":"access_token"
+  "refresh_token":"token"
 }
 
 def job():
