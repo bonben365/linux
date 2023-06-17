724625eef120ca7c9ba996a2baba342c79e5a624
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-17 23:11:50 +0700

    Update imapsync.sh

--- imapsync.sh
+++ imapsync.sh
@@ -132,10 +132,9 @@ then
 
   sudo apt install software-properties-common -y
   sudo add-apt-repository ppa:deadsnakes/ppa
-  sudo apt install python3.9 -y
-
-=======
+  sudo apt install python3 -y
   sudo apt update -y
+  sudo apt install python3-pip -y
   pip3 install requests schedule --user
   pip3 install --upgrade pip
 
