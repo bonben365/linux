a3a29d0f016f512416de5ee1336cb614edb61ea7
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-06-17 23:01:22 +0700

    Update imapsync.sh

--- imapsync.sh
+++ imapsync.sh
@@ -130,9 +130,9 @@ then
   sudo chmod +x imapsync
   sudo cp imapsync /usr/bin/
 
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.9 -y
+  sudo apt install software-properties-common -y
+  sudo add-apt-repository ppa:deadsnakes/ppa
+  sudo apt install python3.9 -y
 
 
   cd /home/imapsync
@@ -149,34 +149,12 @@ fi
 
 if [ $version == "Fedora" ]
 then
-    sudo yum update -y
-    sudo apt install git net-tools -y
-    cd /home
-    sudo git clone https://github.com/kebe7jun/linux-kms-server
-    sudo cp -R /home/linux-kms-server/vlmcsd/ /home/kms
-    sudo mv /home/kms/vlmcsd /home/kms/kmsd
-    cd /home/kms
-    sudo ./kmsd -R170d -L 0.0.0.0:1688 -l /home/kms/kmsd.log
-    firewall-cmd --zone=public --add-port=1688/tcp --permanent
-    echo "@reboot cd /home/kms && sudo ./kmsd -R170d -L 0.0.0.0:1688 -l /home/kms/kms.log" >> /etc/crontab
-    timedatectl set-timezone Asia/Ho_Chi_Minh
-    sudo netstat -ano | grep 1688
+
 fi
 
 if [ $version == "Red" ]
 then
-    sudo yum update -y
-    sudo apt install git net-tools -y
-    cd /home
-    sudo git clone https://github.com/kebe7jun/linux-kms-server
-    sudo cp -R /home/linux-kms-server/vlmcsd/ /home/kms
-    sudo mv /home/kms/vlmcsd /home/kms/kmsd
-    cd /home/kms
-    sudo ./kmsd -R170d -L 0.0.0.0:1688 -l /home/kms/kmsd.log
-    firewall-cmd --zone=public --add-port=1688/tcp --permanent
-    echo "@reboot cd /home/kms && sudo ./kmsd -R170d -L 0.0.0.0:1688 -l /home/kms/kms.log" >> /etc/crontab
-    timedatectl set-timezone Asia/Ho_Chi_Minh
-    sudo netstat -ano | grep 1688
+
 fi
 
 
