47391dcb27fa821a3442cd53721cdab442cfc5ad
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-07-19 21:56:17 +0700

    Update vbgas.sh

--- vbgas.sh
+++ vbgas.sh
@@ -1,7 +1,9 @@
 sudo apt update -y
 sudo apt install virtualbox-guest-additions-iso -y
 sudo mkdir -p /mnt/cdrom
+sudo mkdir -p /tmp/cdrom
 sudo mount /usr/share/virtualbox/VBoxGuestAdditions.iso /mnt/cdrom
-cd /mnt/cdrom
+sudo cp -r /mnt/cdrom/* /tmp/cdrom
+cd /tmp/cdrom
 chmod +x VBoxLinuxAdditions.run
 sudo ./VBoxLinuxAdditions.run
