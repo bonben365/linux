bcd28a3d32f5f59f3830cdd585167757793c1f48
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-07-26 16:35:09 +0700

    Update mautic.sh

--- mautic.sh
+++ mautic.sh
@@ -12,7 +12,7 @@ LC_ALL=C.UTF-8 sudo add-apt-repository ppa:ondrej/php -y
 sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 
-sudo apt install php-imagick php7.4-fpm php7.4-mysql php7.4-common php7.4-gd php7.4-imap php7.4-imap php7.4-curl php7.4-zip php7.4-xml php7.4-mbstring php7.4-bz2 php7.4-intl php7.4-gmp
+sudo apt install php7.4 php-imagick php7.4-fpm php7.4-mysql php7.4-common php7.4-gd php7.4-imap php7.4-imap php7.4-curl php7.4-zip php7.4-xml php7.4-mbstring php7.4-bz2 php7.4-intl php7.4-gmp -y
 
 sudo systemctl start php7.4-fpm && sudo systemctl enable php7.4-fpm
 
